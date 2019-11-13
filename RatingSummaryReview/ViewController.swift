e3fde2b933bcfe049cb28ee47a2ae6985061cccc
Zyad Galal <dev_zyad_galal@yahoo.com>
2019-11-13 09:44:51 +0200

    Rating summary view

--- RatingSummaryReview/ViewController.swift
+++ RatingSummaryReview/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rating5ProgressView: UIProgressView!
+    @IBOutlet weak var rating4ProgressView: UIProgressView!
+    @IBOutlet weak var rating3ProgressView: UIProgressView!
+    @IBOutlet weak var rating2ProgressView: UIProgressView!
+    @IBOutlet weak var rating1ProgressView: UIProgressView!
+    
+    @IBOutlet weak var totalRatingLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let totalRating = 30.0
+        let Stars5 = 15.0
+        let Stars4 = 10.0
+        let Stars3 = 1.0
+        let Stars2 = 1.0
+        let Stars1 = 1.0
+        
+        
+        rating5ProgressView.progress = Float(Stars5 / totalRating)
+        rating4ProgressView.progress = Float(Stars4 / totalRating)
+        rating3ProgressView.progress = Float(Stars3 / totalRating)
+        rating2ProgressView.progress = Float(Stars2 / totalRating)
+        rating1ProgressView.progress = Float(Stars1 / totalRating)
+        totalRatingLabel.text = "\(totalRating) Ratings"
     }
 
 
+    
+    
 }
 
