597ac1250bcfa81b93f26fe9cdc43a03359e08db
Zyad Galal <dev_zyad_galal@yahoo.com>
2019-11-13 08:56:33 +0200

    Initial Commit

---
+++ RatingSummaryReview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RatingSummaryReview
+//
+//  Created by macOS on 11/13/19.
+//  Copyright © 2019 macOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
