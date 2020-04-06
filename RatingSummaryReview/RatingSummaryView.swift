e933f02f3fd0db0890d0623eae3f552c623cca3e
Zyad_Galal <zyad_mg@yahoo.com>
2020-04-06 17:05:43 +0200

    add UIView Extension

--- RatingSummaryReview/RatingSummaryView.swift
+++ RatingSummaryReview/RatingSummaryView.swift
@@ -119,12 +119,4 @@ private extension RatingSummaryView {
     }
 }
 
-extension UIView {
-    /** Loads instance from nib with the same name. */
-    func loadNib() -> UIView {
-        let bundle = Bundle(for: type(of: self))
-        let nibName = type(of: self).description().components(separatedBy: ".").last!
-        let nib = UINib(nibName: nibName, bundle: bundle)
-        return nib.instantiate(withOwner: self, options: nil).first as! UIView
-    }
-}
+
